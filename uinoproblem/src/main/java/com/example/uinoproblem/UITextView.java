5b2198fa29dfce5f41503231281c88b916375d0d
Arvind Rajput <singhsubak9926@gmail.com>
2019-05-09 17:52:00 +0530

    ui librabry add adroid

---
+++ uinoproblem/src/main/java/com/example/uinoproblem/UITextView.java
@@ -0,0 +1,9 @@
+package com.example.uinoproblem;
+
+import android.support.annotation.NonNull;
+
+public class UITextView {
+
+    public static void textViewSetProperties(@NonNull String id, String message, String textSize){ }
+
+}
